e62a0996c8a563b38bf4bdd44bb7c8de16f6a00f
ha <nguyenha24bmt@gmail.com>
2024-06-08 14:50:54 +0700

    ha/storetype crud ver2

--- src/main/java/com/swd391/bachhoasi/service/StoreTypeService.java
+++ src/main/java/com/swd391/bachhoasi/service/StoreTypeService.java
@@ -4,9 +4,9 @@ import com.swd391.bachhoasi.model.dto.request.StoreTypeRequest;
 import com.swd391.bachhoasi.model.dto.response.PaginationResponse;
 import com.swd391.bachhoasi.model.dto.response.StoreLevelResponse;
 import com.swd391.bachhoasi.model.dto.response.StoreTypeResponse;
+import com.swd391.bachhoasi.model.entity.Store;
 import com.swd391.bachhoasi.model.entity.StoreType;
 import org.springframework.data.domain.Pageable;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +14,6 @@ import java.util.Optional;
 public interface StoreTypeService {
     StoreTypeResponse createNewStoreType(StoreTypeRequest storeTypeRequest);
     StoreTypeResponse updateStoreType(BigDecimal id, String name, String description, Boolean status);
-    void updateStoreType(BigDecimal id, String name, String description);
     Optional<StoreType> findById(BigDecimal id);
     PaginationResponse<StoreType> getStoreTypes(Pageable pagination, String keyword);
     StoreTypeResponse deleteStoreTypeById(BigDecimal id);
