ebd547ab2bd54b8ee136b651fd99842cdc1b30e4
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-07-04 03:17:43 +0700

    add some method for menu

--- src/main/java/com/swd391/bachhoasi/controller/MenuController.java
+++ src/main/java/com/swd391/bachhoasi/controller/MenuController.java
@@ -1,10 +1,14 @@
 package com.swd391.bachhoasi.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.swd391.bachhoasi.model.dto.request.ProductMenuRequest;
 import com.swd391.bachhoasi.model.dto.response.ResponseObject;
 import com.swd391.bachhoasi.service.MenuService;
 
@@ -21,6 +25,19 @@ public class MenuController {
             ResponseObject.builder()
             .code("INIT_MENU_SUCCESS")
             .message("Init Menu By Hand Success")
+            .data(result)
+            .isSuccess(true)
+            .status(HttpStatus.OK)
+            .build()
+        );
+    }
+    @PostMapping("/{id}")
+    public ResponseEntity<ResponseObject> addProductToMenu (ProductMenuRequest request, @PathVariable(name = "id") BigDecimal id) {
+        var result = menuService.addProductToMenu(id, request);
+        return ResponseEntity.ok(
+            ResponseObject.builder()
+            .code("ADD_PRODUCT_MENU_SUCCESS")
+            .message("Add Product Menu Success")
             .data(result)
             .isSuccess(true)
             .status(HttpStatus.OK)
