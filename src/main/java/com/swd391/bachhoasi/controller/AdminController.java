e13a17080f41d17fb49a41d61c7e528ad91c6d1c
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-07-12 01:57:28 +0700

    Update request mapping

--- src/main/java/com/swd391/bachhoasi/controller/AdminController.java
+++ src/main/java/com/swd391/bachhoasi/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("admins")
 public class AdminController {
     private final AdminService adminService;
     @GetMapping
